9419d0f21358f0d743154d927922cbb8eec2ae14
Arne Perschke <arne.perschke@web.de>
2020-12-03 12:29:54 +0100

    Added formating to future-proof for days 10 and above

--- src/de/leonm/adventofcode20/Main.java
+++ src/de/leonm/adventofcode20/Main.java
@@ -8,9 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 3; i++) {
-            System.out.println("-------- Day " + i + " --------");
+            String dayNumberCode = String.format("%02d", i);
+            System.out.println("-------- Day " + dayNumberCode + " --------");
             try {
-                Day day = (Day) Class.forName("de.leonm.adventofcode20.days.Day0" + i).getDeclaredConstructor().newInstance();
+                Day day = (Day) Class.forName("de.leonm.adventofcode20.days.Day" + dayNumberCode).getDeclaredConstructor().newInstance();
                 day.printSolutions();
             } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException cne) {
                 System.out.println(cne);
