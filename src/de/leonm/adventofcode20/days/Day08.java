460fe5dcf0ae835b20b9b41449fadf3508345390
lmeister <leonmeister97@gmail.com>
2020-12-09 09:51:45 +0100

    Cleanup, mightve ruined day8 in the process

--- src/de/leonm/adventofcode20/days/Day08.java
+++ src/de/leonm/adventofcode20/days/Day08.java
@@ -3,34 +3,38 @@ package de.leonm.adventofcode20.days;
 import de.leonm.adventofcode20.models.HandheldGameConsole;
 import de.leonm.adventofcode20.models.Instruction;
 import java.io.IOException;
-import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day08 extends Day {
-  List<String> input;
-
+  List<Instruction> instructions;
 
   public Day08() {
     try {
-      input = reader.getStringListFromFile("src/de/leonm/adventofcode20/input/day08.txt");
+      instructions = new ArrayList<>();
+      reader.getStringListFromFile("src/de/leonm/adventofcode20/input/day08.txt")
+          .forEach(l -> instructions.add(new Instruction(l)));
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
   public int partOne() {
-    ArrayDeque<Instruction> instructions = new ArrayDeque<>();
-    input.forEach(i -> instructions.add(new Instruction((i))));
-    HandheldGameConsole gameConsole = new HandheldGameConsole(instructions);
+    int result = 0;
 
-    return gameConsole.run();
+    HandheldGameConsole gameConsole = new HandheldGameConsole(instructions);
+    result = gameConsole.run();
+
+    return result;
   }
+
+
   /**
    * Prints the solutions for the day.
    */
   @Override
   public void printSolutions() {
     System.out.println("Part one: " + partOne());
-    //System.out.println("Part two: " + partTwo());
+    // System.out.println("Part two: " + partTwo());
   }
 }
